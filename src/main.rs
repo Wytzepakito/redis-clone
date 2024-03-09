0445c75914d559ee4d17550277e27761a7cedadd
Wytzepakito <wytzepakito@gmail.com>
2024-03-09 16:36:57 +0100

    Finished stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
-use std::io::prelude::*;
+use std::io::{self, prelude::*};
 use std::net::{TcpListener, TcpStream};
 
-fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
-    let mut buffer = [0; 128];
-    stream.read(&mut buffer)?;
-    stream.write("+PONG\r\n".as_bytes())?;
-    println!("accepted new connection");
-    println!("buffer: {:?}", buffer);
 
-    Ok(())
+const MESSAGE_SIZE: usize = 5;
+
+fn handle_client(mut stream: TcpStream)  {
+
+     let mut buffer = [0; 1024]; // Buffer to store received data
+
+    loop {
+        // Read data from the TcpStream
+        match stream.read(&mut buffer) {
+            Ok(bytes_read) => {
+                if bytes_read == 0 {
+                    // Connection closed
+                    break;
+                }
+
+                // Process the received data (you can replace this with your own logic)
+                let received_data = &buffer[..bytes_read];
+                let string_data = String::from_utf8(received_data.to_vec()).unwrap();
+                println!("Received: {:?}", &string_data);
+
+                // Write back to the TcpStream
+                stream.write_all(b"+PONG\r\n").unwrap();
+            }
+            Err(err) => {
+                eprintln!("Error reading from TcpStream: {}", err);
+                break;
+            }
+        }
+    }
 }
 
 fn main() -> std::io::Result<()> {
