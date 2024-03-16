78303172033f0ec42a94a76b671f39d5ceb40a2c
Wytzepakito <wytzepakito@gmail.com>
2024-03-16 18:01:15 +0100

    Finished stage 9

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,15 @@ use std::net::{TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
 use std::thread;
 
+use clap::Parser;
 use redis_starter_rust::store::RedisDataStore;
 use redis_starter_rust::Redis;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long, default_value_t = 6379)]
+    port: u32
+}
 
 fn handle_stream(mut stream: TcpStream, mut redis: Redis) {
     loop {
@@ -25,9 +32,14 @@ fn handle_expirations(redis: &mut Redis) {
 
 fn main() -> std::io::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
+    let mut args = Args::parse();
+    if args.port > 9999 {
+        println!("Port can't be higher than 9999");
+        args.port = 6379;
+    }
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", args.port)).unwrap();
     let hashmap = Arc::new(Mutex::new(HashMap::new()));
     for stream in listener.incoming() {
         let store = RedisDataStore::new(hashmap.clone());
